b08db875625928f2adf6544f30257e0cbe3b591f
BillFang12 <fzx_ka@126.com>
2018-11-03 23:30:37 +0800

    添加文件

---
+++ src/main/java/com/home/zx/nicehome/NicehomeApplication.java
@@ -0,0 +1,12 @@
+package com.home.zx.nicehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicehomeApplication.class, args);
+	}
+}
