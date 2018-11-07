deec84e0c538fc27720a1ac57ecc64b3b2eb30ad
BillFang12 <fzx_ka@126.com>
2018-11-07 21:48:07 +0800

    并发取数据

--- src/main/java/com/home/zx/nicehome/redis/server/RedisService.java
+++ src/main/java/com/home/zx/nicehome/redis/server/RedisService.java
@@ -1,5 +1,9 @@
 package com.home.zx.nicehome.redis.server;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -24,13 +28,54 @@ public class RedisService {
 	
 	public String sendOrder(String url){
 		//ResponseEntity<TickerModel> model=restTemplate.getForEntity(url, TickerModel.class);
+		long startTime=System.currentTimeMillis();
 		System.out.println("----------------子线程的开始了------"+System.currentTimeMillis());
+		
+		
+		Callable<String> queryUserInfo=new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				System.out.println("----------queryUserInfo------子线程的开始了------"+System.currentTimeMillis());
+				try {
+					Thread.sleep(8000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("----------queryUserInfo------子线程的结束了------"+System.currentTimeMillis());
+				return "queryUserInfo";
+			}
+		};
+		
+		Callable<String> getUserInfo=new Callable<String>() {
+			@Override
+			public String call() throws Exception {
+				System.out.println("----------getUserInfo------子线程的开始了------"+System.currentTimeMillis());
+				try {
+					Thread.sleep(6000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("----------getUserInfo------子线程的结束了------"+System.currentTimeMillis());
+				return "getUserInfo";
+			}
+		};
+		
+		
+		FutureTask<String> taskF=new FutureTask<>(queryUserInfo);
+		FutureTask<String> taskS=new FutureTask<>(getUserInfo);
+		
+		new Thread(taskF).start();
+		new Thread(taskS).start();
+		
 		try {
-			Thread.sleep(6000);
+			taskF.get();
+			taskS.get();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
 		}
-		System.out.println("----------------子线程的结束了------"+System.currentTimeMillis());
+		System.out.println("----------------子线程的结束了------"+(System.currentTimeMillis()-startTime));
 		return "success";
 	}
 	
