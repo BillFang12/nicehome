a41306bcf3ffa7cfecf59c126a48ebee10b6fcc1
BillFang12 <fzx_ka@126.com>
2018-12-04 21:50:03 +0800

    优化代码

--- src/main/java/com/home/zx/nicehome/NicehomeApplication.java
+++ src/main/java/com/home/zx/nicehome/NicehomeApplication.java
@@ -2,10 +2,6 @@ package com.home.zx.nicehome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.thymeleaf.spring4.SpringTemplateEngine;
-import org.thymeleaf.spring4.view.ThymeleafViewResolver;
-import org.thymeleaf.templateresolver.TemplateResolver;
 
 @SpringBootApplication
 public class NicehomeApplication {
@@ -14,26 +10,5 @@ public class NicehomeApplication {
 		SpringApplication.run(NicehomeApplication.class, args);
 	}
 	
-	@Bean
-	public TemplateResolver resolver(){
-		TemplateResolver resoler=new TemplateResolver();
-		resoler.setPrefix("classpath:templates");
-		resoler.setTemplateMode("HTML5");
-		resoler.setSuffix(".html");
-		return resoler;
-	}
 	
-	@Bean
-	public SpringTemplateEngine templateEngine(){
-		SpringTemplateEngine engine=new SpringTemplateEngine();
-		engine.setTemplateResolver(resolver());
-		return engine;
-	}
-	
-	@Bean
-	public ThymeleafViewResolver viewResolver(){
-		ThymeleafViewResolver solver=new ThymeleafViewResolver();
-		solver.setTemplateEngine(templateEngine());
-		return solver;
-	}
 }
--- src/main/java/com/home/zx/nicehome/controller/NiceHomeController.java
+++ src/main/java/com/home/zx/nicehome/controller/NiceHomeController.java
@@ -1,10 +1,10 @@
 package com.home.zx.nicehome.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class NiceHomeController {
 
 	@RequestMapping("/home")
