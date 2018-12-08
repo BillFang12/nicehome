220a6cb714196b9a4da51b254ffac2de7c56786c
BillFang12 <fzx_ka@126.com>
2018-12-08 21:13:41 +0800

    多线程处理消息

--- src/main/java/com/home/zx/nicehome/listener/MqsListener.java
+++ src/main/java/com/home/zx/nicehome/listener/MqsListener.java
@@ -1,5 +1,8 @@
 package com.home.zx.nicehome.listener;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
@@ -15,14 +18,36 @@ public class MqsListener implements MessageListener {
 			msg.getJMSDestination();
 			System.out.println("你收的的數據是："+msg.getJMSTimestamp());
 			System.out.println("我接收到的數據是："+msg.toString());
-			
 			if(msg instanceof ActiveMQTextMessage){
 				ActiveMQTextMessage que=(ActiveMQTextMessage)msg;
 				System.out.println("我的數據是："+que.getText());
+				new Thread(new OperationRunnable(new HashMap<>())).start();
 			}
-			
 		} catch (JMSException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	
+	static class OperationRunnable implements Runnable{
+		ThreadLocal<Map> local=new ThreadLocal<Map>();
+		public OperationRunnable(Map<String,Object> map){
+			local.set(map);
+		}
+		
+		@Override
+		public void run() {
+			long firstTime=System.currentTimeMillis();
+			System.out.println("开始的时间是："+firstTime);
+			Map<String,Object> map=local.get();
+			try {
+				System.out.println("线程启动了。。。。。。。。。。。。。。。。。。。。。。。");
+				Thread.sleep(50000);
+				System.out.println("线程结束了。。。。。。。。。。。。。。。。。。。。。。。");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("执行的时间是："+(System.currentTimeMillis()-firstTime));
 		}
 	}
 
---
+++ src/main/java/com/home/zx/nicehome/redis/server/OperationService.java
@@ -0,0 +1,10 @@
+package com.home.zx.nicehome.redis.server;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OperationService {
+
+	
+	
+}
